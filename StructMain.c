2008f6e1c2961661dd333e5f80a78c42e5c35aa7
gaoxw126 <gaoxw126@126.com>
2014-04-20 20:52:02 +0800

    forth commit

--- StructMain.c
+++ StructMain.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include "StructMain.h"
+
 int 
 main ()
 {
+	
+	int iLoop;
+	int iRtrValue;	
+	int iArrBinSch[10];
 	printf("Struct Algorithm.\n");
-	return 0;
+	/* Call Search files; */
+	/* Binary Search */
+	
+	for( iLoop = 0; iLoop < 10; iLoop++ )
+	{
+		iArrBinSch[iLoop] = iLoop + 5;
+	}
+	iRtrValue = BinarySearch( iArrBinSch, 8, 10 ); 
+	printf("Search Result : %d\n", iRtrValue);
+}
+
+int 
+BinarySearch( const ElementType Arr[], ElementType X, int N)
+{
+	int low, mid , high;
+	low = 0;
+	high = N - 1;
+
+	while( low <= high )
+	{
+		mid = ( low + high )/2;
+
+		if( Arr[mid] > X )
+		{
+			high = mid - 1;
+		}
+		else
+		if ( Arr[mid] < X )
+		{
+			low = mid + 1;
+		}
+		else
+			return mid;
+	}
+	return -1;
 }
